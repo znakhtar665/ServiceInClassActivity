a0953fc8405ea1b1c32a867b8ab59ab45cde7ff1
Karl Morris <karl.morris@temple.edu>
2023-04-13 11:16:04 -0400

    updated button label

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,11 +1,7 @@
 package edu.temple.myapplication
 
-import android.content.ComponentName
-import android.content.Intent
-import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.playButton).setOnClickListener {
+        findViewById<Button>(R.id.startButton).setOnClickListener {
 
         }
 
