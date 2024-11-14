6489fa3ff1a923d661c17bc323c573281f136e09
karl <karl.morris@temple.edu>
2024-11-14 08:05:51 -0500

    Modified implementation

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,57 +1,21 @@
 package edu.temple.myapplication
 
-import android.content.ComponentName
-import android.content.Intent
-import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.IBinder
-import android.os.Looper
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
-
-
-    var timerBinder : TimerService.TimerBinder? = null
-
-    val serviceConnection = object: ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            timerBinder = service as TimerService.TimerBinder
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            timerBinder = null
-        }
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bindService(
-            Intent(this, TimerService::class.java),
-            serviceConnection,
-            BIND_AUTO_CREATE
-        )
-
         findViewById<Button>(R.id.startButton).setOnClickListener {
-            timerBinder?.start(100)
-        }
 
-        findViewById<Button>(R.id.pauseButton).setOnClickListener {
-            timerBinder?.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-            timerBinder?.stop()
-        }
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        unbindService(serviceConnection)
+        }
     }
 }
